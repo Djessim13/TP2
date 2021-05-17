1282efe6679b2ac6ec9f92516e30713d9a3699cf
Allali <djessim.allali@etu.univ-amu.fr>
2021-05-17 10:17:18 +0200

    Exo2

--- src/main/java/fr/univ_amu/iut/exercice2/ApplicationLifeCycle.java
+++ src/main/java/fr/univ_amu/iut/exercice2/ApplicationLifeCycle.java
@@ -6,24 +6,33 @@ import javafx.stage.Stage;
 public class ApplicationLifeCycle extends Application {
 
     public ApplicationLifeCycle() {
+        System.out.println("constructeur ApplicationLifeCycle()");
     }
 
     public static void main(String[] args) {
+        System.out.println("main() : avant launch");
         launch(args);
+        System.out.println("main() : après launch");
     }
 
     @Override
     public void init() throws Exception {
+        System.out.println("init()");
 
     }
 
     @Override
     public void stop() throws Exception {
+        System.out.println("stop()");
 
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Application Life Cycle");
+        System.out.println("start() : avant show stage");
+        primaryStage.show();
+        System.out.println("start() : après show stage");
 
     }
 }
