bbcbe4f1450895edf2abb3d0dec116ff178610f3
Allali <djessim.allali@etu.univ-amu.fr>
2021-05-20 14:37:04 +0200

    Exo9

--- src/main/java/fr/univ_amu/iut/exercice9/EcouteurSimple.java
+++ src/main/java/fr/univ_amu/iut/exercice9/EcouteurSimple.java
@@ -1,5 +1,12 @@
 package fr.univ_amu.iut.exercice9;
 
-public class EcouteurSimple {
+import javafx.event.EventHandler;
+import javafx.event.Event;
 
+public class EcouteurSimple implements EventHandler {
+
+    @Override
+    public void handle(Event event) {
+        System.out.println("Bouton actionné");
+    }
 }
--- src/main/java/fr/univ_amu/iut/exercice9/HelloBeautifulUsefulButton.java
+++ src/main/java/fr/univ_amu/iut/exercice9/HelloBeautifulUsefulButton.java
@@ -2,10 +2,31 @@ package fr.univ_amu.iut.exercice9;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 
 public class HelloBeautifulUsefulButton extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello !");
+        primaryStage.setHeight(250);
+        primaryStage.setWidth(250);
+        BorderPane pane = new BorderPane();
+        Scene scene = new Scene(pane);
+
+        Button hello = new Button("Hello !");
+        hello.setId("buttonHello");
+        pane.setCenter(hello);
+
+        ImageView image = new ImageView("https://raw.githubusercontent.com/IUTInfoAix-M2105/Syllabus/master/assets/logo.png");
+        hello.setGraphic(image);
+
+        hello.setOnAction(new EcouteurSimple());
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 }
